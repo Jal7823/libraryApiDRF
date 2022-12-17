5a0a786e4d895fb35a6ac168db800fa95625a347
Jal7823 <jal7823@gmail.com>
2022-12-17 15:38:30 -0300

    preparing to deploy

--- libraryApiDRF/settings.py
+++ libraryApiDRF/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['http://libraryapidrf.pythonanywhere.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
